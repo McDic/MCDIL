7af74f9b2dadc2baf3bfc767a82e42d3de11ddc2
Minsung Kim <spongbob9876@naver.com>
2023-12-21 06:19:27 +0000

    Fix grammar error

--- mcdil/components/base.py
+++ mcdil/components/base.py
@@ -6,19 +6,8 @@ class AbstractComponent:
     Represents an abstract code component.
     """
 
-    _cache: dict[str, typing.Self]
-
     def __init__(self, component_name: str) -> None:
         self._component_name = component_name
-        if self.component_name in self._cache:
-            raise ValueError(
-                "Same name '%s' of type %s already exists"
-                % (self.component_name, type(self).__name__)
-            )
-        self._cache[self.component_name] = typing.cast(typing.Self, self)
-
-    def __init_subclass__(cls) -> None:
-        cls._cache = {}
 
     def get_commands(self) -> list[str]:
         """
